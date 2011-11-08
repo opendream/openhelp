7f2dc863a6aabc41458dafc4dfceea286d22d2f3
Nat <nat.weerawan@gmail.com>
2011-11-08 15:00:57 +0700

    Add delete link (coordinator item)

--- protected/views/request/_dialog.php
+++ protected/views/request/_dialog.php
@@ -23,9 +23,24 @@
     function addItem() {
         $(this).dialog("close");
         console.log($('#request-form'));
-        // alert( $("#item-name-input").val() + " has been added");
         var inserted_val = $("#item-name-input").val();
         $("item-name-input").html("");
-        $('.coordinator-list').append("<div class='coordinator-item'> <input name='Request[coordinators][]' type='text' value = '"+ inserted_val +"'/></div>");
+        $('.coordinator-list').append("<div class='coordinator-item'> <input name='Request[coordinators][]' type='text' value = '"+ inserted_val +"'/> <span class='detete'><a href='#' alt='delete' class='coordinator-item-delete delete'>delete</a></span></div>");
     }
+    $('.coordinator-item-delete').live('click', function(e) {
+        e.preventDefault();
+        var self = $(this);
+        var coordinate_item = self.parent().parent('div');
+        coordinate_item.remove();
+        
+    });
 </script>
+<!-- 
+<script type="text/coffeescript">
+    window.addItem2 = () -> 
+        $(this).dialog "close"
+        inserted_val = $("#item-name-input").val()
+        $("item-name-input").html ""
+        $('.coordinator-list').append "<div class='coordinator-item'> <input name='Request[coordinators][]' type='text' value ='#{inserted_val}'/></div>"
+</script>
+ -->
