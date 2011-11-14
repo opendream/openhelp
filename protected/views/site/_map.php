db652b8b32b06df5b4863cff6d54fd9e4aaf219e
Nat <nat.weerawan@gmail.com>
2011-11-14 14:06:43 +0700

    Add partial _map view

---
+++ protected/views/site/_map.php
@@ -0,0 +1,9 @@
+<div id="map_canvas" style="width:600px; height:460px"></div>
+
+<?php 
+	$assetsDir = dirname(__FILE__).'/assets';
+	$cs = Yii::app()->getClientScript();
+     // Publishing and registering JavaScript file
+     $cs->registerScriptFile(
+        Yii::app()->assetManager->publish($assetsDir.'/doMap.js'), CClientScript::POS_HEAD);
+ ?>
