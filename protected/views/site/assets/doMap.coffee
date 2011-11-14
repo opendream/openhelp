6e7e1d49594ace1ee4090eff7e128fc9709a508f
Nat <nat.weerawan@gmail.com>
2011-11-14 16:45:53 +0700

    Add marker clustering

--- protected/views/site/assets/doMap.coffee
+++ protected/views/site/assets/doMap.coffee
@@ -3,9 +3,21 @@ window.mapLoadded = (args) ->
   zoom = 12
 
   myOptions = 
-  	zoom: zoom
-  	center: myLatlng
-  	mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: zoom
+    center: myLatlng
+    mapTypeId: google.maps.MapTypeId.ROADMAP
 
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions)
 
+  prefix = "/opendream/openhelp/"
+  $.getJSON "#{prefix}/api/request/?action=index", (nodes) ->
+    markers = []
+    $.each nodes, (id, node) ->
+      placeLatLng = new google.maps.LatLng node?.lat, node?.lng
+      marker = new google.maps.Marker({
+        position: placeLatLng
+        map: map
+        title: node.label
+      })
+      markers.push marker
+    markerCluster = new MarkerClusterer(map, markers);
--- protected/views/site/assets/doMap.js
+++ protected/views/site/assets/doMap.js
@@ -1,13 +1,29 @@
-  function mapLoadded (argument) {
-    var map, myOptions, zoom;
-    var myLatlng;
-      myLatlng = new google.maps.LatLng(13.768, 100.554);
-      zoom = 12;
+(function() {
+  window.mapLoadded = function(args) {
+    var map, myLatlng, myOptions, prefix, zoom;
+    myLatlng = new google.maps.LatLng(13.768, 100.554);
+    zoom = 12;
     myOptions = {
       zoom: zoom,
       center: myLatlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  }
+    prefix = "/opendream/openhelp/";
+    return $.getJSON("" + prefix + "/api/request/?action=index", function(nodes) {
+      var markerCluster, markers;
+      markers = [];
+      $.each(nodes, function(id, node) {
+        var marker, placeLatLng;
+        placeLatLng = new google.maps.LatLng(node != null ? node.lat : void 0, node != null ? node.lng : void 0);
+        marker = new google.maps.Marker({
+          position: placeLatLng,
+          map: map,
+          title: node.label
+        });
+        return markers.push(marker);
+      });
+      return markerCluster = new MarkerClusterer(map, markers);
+    });
+  };
+}).call(this);
