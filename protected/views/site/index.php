013af6c13b3931f9eaf812d922db73c23b514a54
Nat <nat.weerawan@gmail.com>
2011-11-14 17:32:16 +0700

    Clean up

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,30 +1,16 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
-
-
-<?php echo $this->renderPartial('_map', array('model'=>$model)); ?>
-<?php 
-	$assetsDir = dirname(__FILE__).'/assets';
+<?php
 	$cs = Yii::app()->getClientScript();
+	$this->pageTitle=Yii::app()->name;
+	
+	echo $this->renderPartial('_map', array('model'=>$model));
+
      // Publishing and registering JavaScript file
      $cs->registerScriptFile(
         Yii::app()->assetManager->publish($assetsDir.'/coffee-script.js'), CClientScript::POS_HEAD );
-// 	$script = <<<SCRIPT
-// 	window.mapLoadded = function(argument) {
-// 		alert('yo');
-// 	}	
-// SCRIPT;
-// 	$cs->registerScript('amp', $script);
-?>
-
-<?php 
 	echo CGoogleApi::init();
-	// $opts = ".CJavaScript::encode($options);
+
 	$gload = <<<GLOAD
 	google.load("maps","3",{'callback':'mapLoadded','other_params':'sensor=false'});
 GLOAD;
-
 	$cs->registerScript('gload', $gload);
-
 ?>
-
-
