e40521ac559d63ce177043971ab5fe61e43b8b7f
Nat <nat.weerawan@gmail.com>
2011-11-15 16:19:34 +0700

    Add map to locationView

--- protected/views/request/_database_detail_map.php
+++ protected/views/request/_database_detail_map.php
@@ -1,3 +1,10 @@
-<section id="map">
-  map
-</section>
+<div id="map_canvas" style="width:600px; height:460px"></div>
+
+<?php 
+	$assetsDir = dirname(__FILE__).'/assets';
+	$cs = Yii::app()->getClientScript();
+     // Publishing and registering JavaScript file
+     $cs->registerScriptFile(
+        Yii::app()->assetManager->publish($assetsDir.'/doMap.js'), CClientScript::POS_HEAD);
+
+ ?>
--- protected/views/request/database.php
+++ protected/views/request/database.php
@@ -4,5 +4,21 @@
 	echo $this->renderPartial('_database_detail_need', array('items' => $items	));
 	echo $this->renderPartial('_database_detail_coordinators', array('coordinators' => $coordinators,
 	 'journey'=>$extra['journey'], 'remark' => $extra['remark']));
+	$assetsDir = dirname(__FILE__).'/assets';
+	$cs = Yii::app()->getClientScript();
+     // Publishing and registering JavaScript file
+     $cs->registerScriptFile(
+     Yii::app()->assetManager->publish($assetsDir.'/coffee-script.js'), CClientScript::POS_HEAD );
+	echo CGoogleApi::init();
+
+	$gload = <<<GLOAD
+	google.load("maps","3",{'callback':'mapLoadded','other_params':'sensor=false'});
+GLOAD;
+	$cs->registerScript('gload', $gload);
+	$basePath = Yii::app()->getRequest()->getBaseUrl();
+	$options = array('basePath' => $basePath);
+
+	$js_settings_str = CJavaScript::encode($options);
+	echo CHtml::Script('var Yii= Yii || {}; Yii.settings = ' . $js_settings_str);		 
 ?>
 	
