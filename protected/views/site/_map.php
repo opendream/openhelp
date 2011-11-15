5ebc7c7c68abc5caef2b27da84b7691409bef3e9
Nat <nat.weerawan@gmail.com>
2011-11-15 19:33:51 +0700

    Move some code to _map

--- protected/views/site/_map.php
+++ protected/views/site/_map.php
@@ -5,6 +5,22 @@
 	$cs = Yii::app()->getClientScript();
      // Publishing and registering JavaScript file
      $cs->registerScriptFile(
+        Yii::app()->assetManager->publish($assetsDir.'/coffee-script.js'), CClientScript::POS_HEAD );
+	echo CGoogleApi::init();
+
+	$gload = <<<GLOAD
+	google.load("maps","3",{'callback':'mapLoadded','other_params':'sensor=false'});
+GLOAD;
+	$cs->registerScript('gload', $gload);
+	$basePath = Yii::app()->getRequest()->getBaseUrl();
+	$options = array('basePath' => $basePath);
+
+	$js_settings_str = CJavaScript::encode($options);
+	echo CHtml::Script('var Yii= Yii || {}; Yii.settings = ' . $js_settings_str);	
+	$assetsDir = dirname(__FILE__).'/assets';
+	$cs = Yii::app()->getClientScript();
+     // Publishing and registering JavaScript file
+     $cs->registerScriptFile(
         Yii::app()->assetManager->publish($assetsDir.'/doMap.js'), CClientScript::POS_HEAD);
         
      $cs->registerScriptFile(
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,22 +1,6 @@
 <?php
-	$assetsDir = dirname(__FILE__).'/assets';
-	$cs = Yii::app()->getClientScript();
+
 	$this->pageTitle=Yii::app()->name;
 
 	$this->renderPartial('_map');
-
-     // Publishing and registering JavaScript file
-     $cs->registerScriptFile(
-        Yii::app()->assetManager->publish($assetsDir.'/coffee-script.js'), CClientScript::POS_HEAD );
-	echo CGoogleApi::init();
-
-	$gload = <<<GLOAD
-	google.load("maps","3",{'callback':'mapLoadded','other_params':'sensor=false'});
-GLOAD;
-	$cs->registerScript('gload', $gload);
-	$basePath = Yii::app()->getRequest()->getBaseUrl();
-	$options = array('basePath' => $basePath);
-
-	$js_settings_str = CJavaScript::encode($options);
-	echo CHtml::Script('var Yii= Yii || {}; Yii.settings = ' . $js_settings_str);	
 ?>
