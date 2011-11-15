98d46a652b96bb80968ff38ce5958529c001f57f
Nat <nat.weerawan@gmail.com>
2011-11-15 19:32:37 +0700

    render villages list

---
+++ protected/views/request/_database_detail_villages.php
@@ -0,0 +1,8 @@
+<section id="st-village" class="drop-shadow curved curved-hz-1">
+    <header><h2>หมู่บ้านภายในตำบลนี้</h2></header>
+    <ul>
+    <?php foreach ($location_extra0s as $key => $village): ?>
+      <li><a href=""><?php echo $village['label'] ?></a></li>
+    <?php endforeach ?>
+    </ul>
+  </section> <!-- end st-vilage-->
--- protected/views/request/locationView.php
+++ protected/views/request/locationView.php
@@ -4,7 +4,7 @@
 	echo $this->renderPartial('_database_detail_need', array('items' => $items	));
 	echo $this->renderPartial('_database_detail_coordinators', array('coordinators' => $coordinators,
 	 'journey'=>$extra['journey'], 'remark' => $extra['remark'], 'location_detail' => $location));
-
+	echo $this->renderPartial('_database_detail_villages', array('location_extra0s' => $location_extra0s));
 	$assetsDir = dirname(__FILE__).'/assets';
 	$cs = Yii::app()->getClientScript();
      // Publishing and registering JavaScript file
