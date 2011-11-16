3bc0ce058ec9962ff9c6c95d0659d181242684b8
Nat <nat.weerawan@gmail.com>
2011-11-16 10:35:27 +0700

    Fix bug constant

--- protected/views/request/locationView.php
+++ protected/views/request/locationView.php
@@ -1,6 +1,6 @@
 <div id="content">
 <?php
-	echo $this->renderPartial('_database_detail_lastupdate', array('extraDouble' => $extraDouble, location_text => $location_text));
+	echo $this->renderPartial('_database_detail_lastupdate', array('extraDouble' => $extraDouble, 'location_text' => $location_text));
 	echo $this->renderPartial('_database_detail_map', array('location_id' => $location_id));
 	echo $this->renderPartial('_database_detail_need', array('items' => $items	));
 	$coordinators_params = array(
