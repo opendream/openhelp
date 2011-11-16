850796ece1ed9d496eead27271563ee7198e8422
Nat <nat.weerawan@gmail.com>
2011-11-16 09:35:33 +0700

    Change representation data

--- protected/views/request/_database_detail_lastupdate.php
+++ protected/views/request/_database_detail_lastupdate.php
@@ -1,5 +1,5 @@
 <article id="entry-1" class="entry">
-<header><h1>ต.เกาะเกิด อ.บางปะอิน จ.อยุธยา</h1></header>
+<header><h1><?php print $location_text ?></h1></header>
 <section class="st-update drop-shadow curved curved-hz-1">
 <div id="entry-date">
     <span class="year">2011</span>
@@ -9,8 +9,14 @@
 
   <div class="entry-update extra-double">
     <header><h2>อัพเดทล่าสุด</h2></header>
-    <p class="extra-double-lebel-0">จำนวนครัวเรือน <strong><?php echo $extraDouble['sum']['sum_request.extra_double0'][0]['sum_extra_double0']; ?></strong></p>
-			<p class="extra-double-lebel-1">ระดับน้ำ (เมตร)<strong><?php echo $extraDouble['water_level']['request.extra_double2']['min']; ?></strong></p>
-			<p class="extra-double-lebel-2">ท่วมมาแล้ว (วัน) <strong>30</strong></p>
+    <?php 
+    	$family_num = $extraDouble['sum']['sum_request.extra_double0'][0]['sum_extra_double0']; 
+    	$people_num = $extraDouble['sum']['sum_request.extra_double1'][0]['sum_extra_double1'];
+    
+     ?>
+
+    <p class="extra-double-lebel-0">จำนวนครัวเรือน <strong><?php echo $family_num ?></strong></p>
+	<p class="extra-double-lebel-2">จำนวนประชากร <strong><?php echo $people_num ?></strong></p>
+	<p class="extra-double-lebel-1">ระดับน้ำ (เมตร)<strong><?php echo $extraDouble['water_level']['request.extra_double2']['min']; ?></strong></p>
   </div> <!-- /entry-update -->
 </section> <!-- /st-detail -->
