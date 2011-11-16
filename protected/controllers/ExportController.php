4e30df8c779822df064ef05797d32978e0de732f
crosalot <crosalot@gmail.com>
2011-11-16 16:32:52 +0700

    fuck layout

--- protected/controllers/ExportController.php
+++ protected/controllers/ExportController.php
@@ -11,17 +11,31 @@ class ExportController extends Controller
 	/**
 	 * Lists all models.
 	 */
-	public function actionLocation()
+	public function actionLocation($id)
 	{
+	  /*
+	  header("Content-type: application/csv");
+    header("Content-Disposition: attachment; filename=file.csv");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    */
+    
+    $col = array(
+      array('col0' => '0-0', 'col1' => '0-1', 'col2' => '0-2'), 
+      array('col0' => '1-0', 'col1' => '1-1', 'col2' => '1-2'),
+      array('col0' => '2-0', 'col1' => '2-1', 'col2' => array()),
+    );
+    
 	  $rows = array(
-      array('col0' => '0-0', 'col1' => '0-1', 'col1' => '0-2'), 
-      array('col0' => '1-0', 'col1' => '1-1', 'col1' => '1-2'),
-      array('col0' => '2-0', 'col1' => '2-1', 'col1' => '2-2'),
+      array('col0' => '0-0', 'col1' => '0-1', 'col2' => '0-2'), 
+      array('col0' => '1-0', 'col1' => '1-1', 'col2' => '1-2'),
+      array('col0' => '2-0', 'col1' => '2-1', 'col2' => $col),
     );
 	  $this->export($rows);
+	  exit();
 	}
 	
-	public function export($rows, $addHeader=false, $sCol=',', $sRow="\n")
+	public function export($rows, $addHeader=true, $sCol=',', $sRow="\n", $merge=false)
 	{
     
     // Header ============================================
@@ -36,13 +50,13 @@ class ExportController extends Controller
       foreach(Yii::app()->params['location'] as $key) {
         $labels[$key] = Yii::t('locale', $key);
       }
-      foreach (Yii::app()->params['extra_location'] as $key => $value) {
+      foreach (Yii::app()->params['request']['extra']['location'] as $key => $value) {
         $labels['extra_location'.$key] = $value['label'];
       }
-      foreach (Yii::app()->params['extra_double'] as $key => $value) {
+      foreach (Yii::app()->params['request']['extra']['double'] as $key => $value) {
         $labels['extra_double'.$key] = $value['label'];
       }
-      foreach (Yii::app()->params['extra_text'] as $key => $value) {
+      foreach (Yii::app()->params['request']['extra']['text'] as $key => $value) {
         $labels['extra_text'.$key] = $value['label'];
       }
 
@@ -52,22 +66,27 @@ class ExportController extends Controller
         $header[$col] = isset($labels[$col])? $labels[$col]: $col;
       }
       $header['end'] = '""';
+      $header = array(-1 => $header);
       $rows = $header + $rows;
     }
-
     
     // Data ============================================    
     foreach ($rows as $row) {
       foreach ($row as $col => $value) {
         if (is_array($value)) {
-          $this->export($value, false, ':', '/');
+          $this->export($value, false, ':', '/', true);
         }
         else {
-          echo '"'.$value.'"'.$sCol;
+          if ($merge) {
+            # code...
+          }
+          else {
+            echo '"'.$value.'"'.$sCol;
+          }
         }
       }
-      echo '""'.$sRow;
-    }
+      //echo '""'.$sRow;
+    }    
   }
 
 
--- protected/controllers/RequestController.php
+++ protected/controllers/RequestController.php
@@ -6,7 +6,7 @@ class RequestController extends Controller
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/base';
+	public $layout='//layouts/column2';
 
 	/**
 	 * @return array action filters
