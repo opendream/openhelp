a3b96247447bc20c5b605a444a5b6d255ac6f7d0
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2011-11-18 18:20:46 +0700

    add test config and testcase

---
+++ protected/tests/fixtures/request.php
@@ -0,0 +1,19 @@
+<?php
+	return array(
+		'request1' => array(
+			'detail' => 'aaa',
+    		'location_id' => 1,
+        	'extra_text4' => 'extra_text4',
+        	'extra_text5' => 'extra_text5',
+        	'extra_text6' => 'extra_text6',
+        	'extra_text9' => 'extra_text7',
+
+        	'extra_double0' => 250,
+        	'extra_double1' => 499,
+        	'extra_double2' => 1.8,
+        	'extra_location0' => 1,
+        	'date_created' => '2011-11-01',
+        	'last_updated' => '2011-11-01',
+        	'status' => 0,
+    		),	
+	);
---
+++ protected/tests/fixtures/request_coordinator.php
@@ -0,0 +1,11 @@
+<?php
+	return array(
+		'requestCoordinator1' => array(
+			'request_id' => 1,
+			'coordinator_id' => 1,
+		),
+		'requestCoordinator2' => array(
+			'request_id' => 1,
+			'coordinator_id' => 2,
+		),
+	);
