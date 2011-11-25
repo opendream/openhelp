2eec4024cd1965aad9c01fbb91c6492be9decf1e
crosalot <crosalot@gmail.com>
2011-11-25 15:38:52 +0700

    complete style webform

--- protected/views/webform/_view.php
+++ protected/views/webform/_view.php
@@ -1,20 +1,42 @@
-<div class="view">
-  <b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-	
-  <?php echo LocationHtml::locationView($data->location_id, array(
-    'style' => 'endLevelLink',
-    'link' => CController::createUrl("/webform/view/".$data->id),
-  )); ?>
+<div class="db-list">
+  
+  <hgroup class="db-item1">
 
-	<b><?php echo t(CHtml::encode($data->getAttributeLabel('date_created'))); ?>:</b>
-	<?php echo CHtml::encode($data->date_created); ?>
-	<br />
-	
-	<b><?php echo t('Author'); ?>:</b>
-	<?php echo CHtml::encode($data->user->username); ?>
-	<br />
+    <table>
+      <tbody>
+      <tr>
+        <td class="extra-double-label">
+          <?php echo t('ID'); ?>
+        </td>
+        <td class="extra-double-input">
+            <?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+        </td>
+      </tr>
+      <tr>
+        <td class="extra-double-label">
+          <?php echo t('Date'); ?>
+        </td>
+        <td class="extra-double-input">
+          <?php echo CHtml::encode(date('d/m/Y', strtotime($data->date_created))); ?>
+        </td>
+      </tr>
+      <tr>
+        <td class="extra-double-label">
+          <?php echo t('Author'); ?>
+        </td>
+        <td class="extra-double-input">
+          <?php echo CHtml::encode($data->user->username); ?>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+	</hgroup> <!-- /db-item1 -->
+  <div class="db-item2">
+    <?php echo LocationHtml::locationView($data->location_id, array(
+      'style' => 'endLevelLink',
+      'link' => CController::createUrl("/webform/view/".$data->id),
+    )); ?>
+  </div>
 
 
 </div>
